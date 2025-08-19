0586a7fa770f6742e783aeb904644ed9c7a64c8a
Ale Casas <alejandrofcasas@hotmail.com>
2025-08-19 14:17:33 -0300

    first

--- obj/Debug/net8.0/Wheelzy.AssemblyInfo.cs
+++ obj/Debug/net8.0/Wheelzy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wheelzy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47f55028285715ae4afdbf8af498f45b95877d20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wheelzy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wheelzy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
